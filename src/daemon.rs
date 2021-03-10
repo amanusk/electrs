5120c4bd8cb04fe7bc5fbc138d6465b88aa45e74
amanusk <amanusk@kzencorp.com>
2021-03-10 13:39:36 +0200

    working on adding dogecoin support

--- src/daemon.rs
+++ src/daemon.rs
@@ -22,7 +22,11 @@ use crate::errors::*;
 use crate::metrics::{HistogramOpts, HistogramVec, Metrics};
 use crate::signal::Waiter;
 use crate::util::HeaderList;
+use bitcoin::consensus::encode::Decodable;
 use bitcoin::BitcoinHash;
+
+// #[macro_use]
+// use crate::js_try;
 
 fn parse_hash<T: Hash>(value: &Value) -> Result<T> {
     Ok(T::from_hex(
@@ -33,15 +37,33 @@ fn parse_hash<T: Hash>(value: &Value) -> Result<T> {
     .chain_err(|| format!("non-hex value: {}", value))?)
 }
 
-fn header_from_value(value: Value) -> Result<BlockHeader> {
+fn header_from_value(value: Value, network: Network) -> Result<BlockHeader> {
     let header_hex = value
         .as_str()
         .chain_err(|| format!("non-string header: {}", value))?;
     let header_bytes = hex::decode(header_hex).chain_err(|| "non-hex header")?;
-    Ok(
-        deserialize(&header_bytes)
-            .chain_err(|| format!("failed to parse header {}", header_hex))?,
-    )
+    if network == Network::Dogecoin {
+        let header_bytes = hex::decode(header_hex).chain_err(|| "non-hex header")?;
+        let mut cur = std::io::Cursor::new(&header_bytes);
+        let block_header: bitcoin::BlockHeader =
+            Decodable::consensus_decode(&mut cur).chain_err(|| "Unable to decode header")?;
+        Ok(block_header)
+    } else {
+        Ok(deserialize(&header_bytes)
+            .chain_err(|| format!("failed to parse header {}", header_hex))?)
+    }
+}
+
+// pub fn processDogeBlock(block: String, network: Network) -> Result<JsValue, JsValue> {
+pub fn processDogeBlock(value: Value, network: Network) -> Result<BlockHeader> {
+    let header_hex = value
+        .as_str()
+        .chain_err(|| format!("non-string header: {}", value))?;
+    let header_bytes = hex::decode(header_hex).chain_err(|| "non-hex header")?;
+    let mut cur = std::io::Cursor::new(&header_bytes);
+    let block_header: bitcoin::BlockHeader =
+        Decodable::consensus_decode(&mut cur).chain_err(|| "Unable to decode header")?;
+    Ok(block_header)
 }
 
 fn block_from_value(value: Value) -> Result<Block> {
@@ -474,7 +496,8 @@ impl Daemon {
 
     pub fn estimatesmartfee(&self, conf_target: usize, estimate_mode: &str) -> Result<f64> {
         let val: Value = self.request("estimatesmartfee", json!([conf_target, estimate_mode]))?;
-        let fee_estimate: Result<FeeEstimate> = Ok(from_value(val).chain_err(|| "invalid fee estimate")?);
+        let fee_estimate: Result<FeeEstimate> =
+            Ok(from_value(val).chain_err(|| "invalid fee estimate")?);
         Ok(fee_estimate?.feerate)
     }
 
@@ -483,10 +506,13 @@ impl Daemon {
     }
 
     pub fn getblockheader(&self, blockhash: &BlockHash) -> Result<BlockHeader> {
-        header_from_value(self.request(
-            "getblockheader",
-            json!([blockhash.to_hex(), /*verbose=*/ false]),
-        )?)
+        header_from_value(
+            self.request(
+                "getblockheader",
+                json!([blockhash.to_hex(), /*verbose=*/ false]),
+            )?,
+            self.network,
+        )
     }
 
     pub fn getblockheaders(&self, heights: &[usize]) -> Result<Vec<BlockHeader>> {
@@ -498,7 +524,7 @@ impl Daemon {
             .collect();
         let mut result = vec![];
         for h in self.requests("getblockheader", &params_list)? {
-            result.push(header_from_value(h)?);
+            result.push(header_from_value(h, self.network)?);
         }
         Ok(result)
     }
@@ -673,5 +699,31 @@ impl Daemon {
         trace!("downloaded {} block headers", new_headers.len());
         new_headers.reverse(); // so the tip is the last vector entry
         Ok(new_headers)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bitcoin::network::constants::Network;
+    use hex::decode as hex_decode;
+
+    #[test]
+    fn deserialize_doge_header() {
+        let some_header = "02006200c7200ba555cbde915159e8d3e341822fa2c9c027ed2bd8bd07324206c48fd58a77d834db30a5d77351164eb7deb15f9a1ba896b8f7c89c23798ed94fb43b50a039fb115475df2f1be8afc4ca";
+
+        let header: BlockHeader = header_from_value(json!(some_header), Network::Dogecoin)
+            .expect("Can't deserialize correct block header");
+
+        println!("{:?}", header);
+    }
+
+    #[test]
+    fn deserialize_auxpow_doge_header() {
+        // let some_header = "02006200c7200ba555cbde915159e8d3e341822fa2c9c027ed2bd8bd07324206c48fd58a77d834db30a5d77351164eb7deb15f9a1ba896b8f7c89c23798ed94fb43b50a039fb115475df2f1be8afc4ca";
+        let some_header = "020162000d6f03470d329026cd1fc720c0609cd378ca8691a117bd1aa46f01fb09b1a8468a15bf6f0b0e83f2e5036684169eafb9406468d4f075c999fb5b2a78fbb827ee41fb11548441361b0000000001000000010000000000000000000000000000000000000000000000000000000000000000ffffffff380345bf09fabe6d6d980ba42120410de0554d42a5b5ee58167bcd86bf7591f429005f24da45fb51cf0800000000000000cdb1f1ff0e000000ffffffff01800c0c2a010000001976a914aa3750aa18b8a0f3f0590731e1fab934856680cf88ac00000000b3e64e02fff596209c498f1b18f798d62f216f11c8462bf3922319000000000003a979a636db2450363972d211aee67b71387a3daaa3051be0fd260c5acd4739cd52a418d29d8a0e56c8714c95a0dc24e1c9624480ec497fe2441941f3fee8f9481a3370c334178415c83d1d0c2deeec727c2330617a47691fc5e79203669312d100000000036fa40307b3a439538195245b0de56a2c1db6ba3a64f8bdd2071d00bc48c841b5e77b98e5c7d6f06f92dec5cf6d61277ecb9a0342406f49f34c51ee8ce4abd678038129485de14238bd1ca12cd2de12ff0e383aee542d90437cd664ce139446a00000000002000000d2ec7dfeb7e8f43fe77aba3368df95ac2088034420402730ee0492a2084217083411b3fc91033bfdeea339bc11b9efc986e161c703e07a9045338c165673f09940fb11548b54021b58cc9ae5";
+
+        let header: BlockHeader = header_from_value(json!(some_header), Network::Dogecoin)
+            .expect("Can't deserialize correct block header");
+        println!("{:?}", header);
     }
 }
